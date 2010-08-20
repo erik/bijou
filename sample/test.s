423fcb917871febfe179dbe4ca36463d24c791c7
Erik Price <erik.price16@gmail.com>
2010-08-19 20:00:15 -0400

    Cleaned some commented out code

--- sample/test.s
+++ sample/test.s
@@ -11,14 +11,14 @@
 ; code section
 >CODE
 
-   ; loadk 0 2     ; r[0] = 42
-   ; add 1 0 1K    ; r[1] = 42 + 5
-   ; setglobal 1 0 ; GBL[K[0]] = R[1]
-   ; getglobal 0 0 ; R[0] = GBL[K[0]]
+    loadk 0 2     ; r[0] = 42
+    add 1 0 1K    ; r[1] = 42 + 5
+    setglobal 1 0 ; GBL[K[0]] = R[1]
+    getglobal 0 0 ; R[0] = GBL[K[0]]
     closure 0 1K  ; println
     loadk 1 3     ;  "A string!"
     call 2 0 1    ;
-    return 2      ; 47
+    return 2      ; NULL
 
 <CODE
 
--- vm/string.c
+++ vm/string.c
@@ -11,12 +11,6 @@ BijouString BijouString_new(char * str)
 {
     BijouString bs;
 
-    /*    if (str == NULL)*/
-    /*        bs.ptr = NULL;*/
-    /*    else {*/
-    /*        bs.ptr = B_MALLOC(strlen(str));*/
-    /*        strcpy(bs.ptr, str);*/
-    /*    }*/
     bs.ptr = str;
     bs.len = str == NULL ? 0 : strlen(str);
     return bs;
